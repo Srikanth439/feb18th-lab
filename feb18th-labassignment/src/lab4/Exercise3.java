156a044d0af50d813e3d35b62fb2a65aafc8dcea
Srikanth <Srikanth@srikanth>
2022-03-01 22:36:15 +0530

    lab4

--- feb18th-labassignment/src/lab4/Exercise3.java
+++ feb18th-labassignment/src/lab4/Exercise3.java
@@ -4,6 +4,11 @@ abstract class item{
 	private String title;
 	private int copies;
 	private static boolean Checkedin;
+	item(double id, String title, int copies){
+		this.Id=id;
+		this.title=title;
+		this.copies=copies;
+	}
 	public static void setCheckedin(boolean checkedin) {
 		checkedin=checkedin;
 	}
@@ -34,6 +39,70 @@ abstract class item{
 	
 	
 }
+abstract class WrittenItem extends item{
+	WrittenItem(double id, String title, int copies) {
+		super(id, title, copies);
+		// TODO Auto-generated constructor stub
+	}
+
+	private String author;
+}
+//////////////////////////////////////////////////////
+class Book extends WrittenItem{
+
+	Book(double id, String title, int copies) {
+		super(id, title, copies);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
+///////////////////////////////////////////////
+class JournalPaper extends WrittenItem{
+	private int year;
+
+	JournalPaper(double id, String title, int copies) {
+		super(id, title, copies);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
+//////////////////////////////////////////
+abstract class Mediaitem extends item{
+	private int runtime;
+
+	Mediaitem(double id, String title, int copies) {
+		super(id, title, copies);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
+////////////////////////////////////////
+
+class Vedio extends Mediaitem{
+	private String director;
+	private String genre;
+	private int pub_year;
+
+	Vedio(double id, String title, int copies) {
+		super(id, title, copies);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
+////////////////////////////////////////
+class CD extends Mediaitem{
+	private String artist;
+	private String genre;
+
+	CD(double id, String title, int copies) {
+		
+		super(id, title, copies);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
+
+
 public class Exercise3 {
 	
 
